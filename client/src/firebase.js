6182672aad833e39ed8fe6f5ccd70b41b95e8cd9
rafogi <raffycordero2001@yahoo.ca>
2020-12-07 19:07:40 -0800

    clean react add firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDg6CYt6YOy57XeUw1S_hbLGaU0SoEEh4Q",
+  authDomain: "whatsappclone-8bc4f.firebaseapp.com",
+  projectId: "whatsappclone-8bc4f",
+  storageBucket: "whatsappclone-8bc4f.appspot.com",
+  messagingSenderId: "87343275396",
+  appId: "1:87343275396:web:5766fdf30fbefd759511d1",
+  measurementId: "G-NY0XMXKVF6"
+};
