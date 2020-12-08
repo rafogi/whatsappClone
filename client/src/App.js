3264c94fa7f8a7e809fb99766d29e23ae0b5dada
rafogi <raffycordero2001@yahoo.ca>
2020-12-07 20:18:12 -0800

    add chat and sidebar comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+
+      <Sidebar/>
+      <Chat/>
     </div>
   );
 }
