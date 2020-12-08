1268be53619b1aaf35a21174dc78f214dbd9512f
rafogi <raffycordero2001@yahoo.ca>
2020-12-07 20:52:57 -0800

    add icons to sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import Chat from './Chat';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-
-      <Sidebar/>
-      <Chat/>
+      <div className="app__body">
+        <Sidebar/>
+        <Chat/>
+      </div>
     </div>
   );
 }
--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./Sidebar.css"
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import {Avatar, IconButton} from "@material-ui/core";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>I am a sidebar</h1>
+      <div className="sidebar__header">
+        <div className="sidebar__headerRight">
+          <IconButton>
+          <DonutLargeIcon />
+
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
