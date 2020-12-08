9f565d45cc1c507e420ec9095feae02988b3f228
rafogi <raffycordero2001@yahoo.ca>
2020-12-08 10:33:22 -0800

    format css for upper right sidebar

--- client/src/Sidebar.js
+++ client/src/Sidebar.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import "./Sidebar.css"
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
 import {Avatar, IconButton} from "@material-ui/core";
+
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
+        <Avatar src="https://avatars2.githubusercontent.com/u/68399853?s=400&u=0c0dd963c222d62cd444ceba09a55518acdc30c2&v=4"/>
         <div className="sidebar__headerRight">
           <IconButton>
           <DonutLargeIcon />
-
           </IconButton>
+          <IconButton>
+          <ChatIcon/>
+          </IconButton>
+          <IconButton>
+          <MoreVertIcon/>
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined/>
+          <input placeholder="Search or start new chat" type="text"/>
         </div>
       </div>
     </div>
