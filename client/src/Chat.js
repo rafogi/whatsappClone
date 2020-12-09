236f0f50c8c9ee1a860512403e9e9a6ef4a40b0b
rafogi <raffycordero2001@yahoo.ca>
2020-12-08 16:10:29 -0800

    add chat body

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,10 +1,43 @@
+import { Avatar, IconButton } from '@material-ui/core';
 import React from 'react';
 import "./Chat.css"
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons"
+
 
 const Chat = () => {
   return (
-    <div>
-      <h1>chat</h1>
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">
+            Raffy
+          </span>
+          hello world
+          <span className="chat__timestamp">
+            {new Date().toLocaleString()}
+          </span>
+
+
+        </p>
+      </div>
     </div>
   );
 };
