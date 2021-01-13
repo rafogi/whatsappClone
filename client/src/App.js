54d1422525fa64fb957641e96fbefafb5296a7c0
rafogi <raffycordero2001@yahoo.ca>
2021-01-12 20:21:10 -0800

    add pusher

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import React, {useEffect} from "react";
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
+import Pusher from 'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('c53a916f4cf3da7628db', {
+      cluster: 'us3'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="App">
       <div className="app__body">
