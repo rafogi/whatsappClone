779addc34de982e60c1ee8908e98876b9c66b8ee
rafogi <raffycordero2001@yahoo.ca>
2021-01-12 20:39:19 -0800

    add axios and pusher to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Pusher from 'pusher-js';
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/messages/sync')
+      .then(response => {
+        setMessages(response.data);
+      })
+  }, []);
 
   useEffect(() => {
     const pusher = new Pusher('c53a916f4cf3da7628db', {
@@ -12,10 +21,18 @@ function App() {
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', function(data) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage])
     });
-  }, [])
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+
+  }, [messages]);
+
+  console.log(messages);
 
   return (
     <div className="App">
