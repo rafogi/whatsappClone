810ee4c2411496e90a470eaaaeef1603d035877c
rafogi <raffycordero2001@yahoo.ca>
2021-01-14 19:58:28 -0800

    add firebase googleauth

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDg6CYt6YOy57XeUw1S_hbLGaU0SoEEh4Q",
   authDomain: "whatsappclone-8bc4f.firebaseapp.com",
@@ -7,3 +9,14 @@ const firebaseConfig = {
   appId: "1:87343275396:web:5766fdf30fbefd759511d1",
   measurementId: "G-NY0XMXKVF6"
 };
+
+const firebaseApp= firebase.initializeAPP
+(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.
+GoogleAuthProvider();
+
+export {auth, provider};
+export default db;
