e5c7d0a9e549639ec6bcc456a84989fc38eb04e8
rafogi <raffycordero2001@yahoo.ca>
2021-01-18 11:57:26 -0800

    add proxy to frontend axios

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import Pusher from 'pusher-js';
-import axios from "./axios";
+import axios from "axios";
 
 function App() {
   const [messages, setMessages] = useState([]);
